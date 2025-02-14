4209ceed5d43e7b110960c28fdf1222a1580b027
unknown <alex41610@gmail.com>
2025-02-14 14:48:44 +0800

    chore: switch to React frontend stack

--- src/calendar-web/vite.config.js
+++ src/calendar-web/vite.config.js
@@ -1,20 +1,7 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src')
-    }
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5129',
-        changeOrigin: true
-      }
-    }
-  }
+  plugins: [react()],
 })
